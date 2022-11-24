a8f6a6c425b3cff50a4e809b6c33bc94ecbfe547
Kaik Selhorst <contato.kaikselhorst@gmail.com>
2022-11-24 09:55:42 -0400

    Feat: new Vercel Api external

--- src/mixins.js
+++ src/mixins.js
@@ -6,7 +6,9 @@ export const api = {
   },
   methods: {
     async getApi(query) {
-      const response = await fetch(`http://localhost:3000/${query}`);
+      const response = await fetch(
+        `https://koorst-api.vercel.app/api/${query}`
+      );
       const data = await response.json();
       this.dadosApi = data;
       return;
